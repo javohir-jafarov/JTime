7bf1a2c734ddc33094c2605c080c4bb303eb4510
javohir-jafarov <jafarjavohir2010@gmail.com>
2026-05-17 18:37:54 +0500

    complate the UI

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-null not found
+let day = document.querySelector('.day')
+let hour = document.querySelector('.hour')
+let min = document.querySelector('.min')
+let sec = document.querySelector('.sec')
+
+console.log(new Date.year)
